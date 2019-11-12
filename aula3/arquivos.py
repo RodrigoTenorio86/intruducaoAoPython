a3e673f69c94c73a1dbbcc2ae7603cb2a2ad7f6c
Rodrigo Tenório de Boa Ventura <RM333918@fiap.com.br>
2019-11-11 22:13:28 -0300

    desenvolvimento de exercicio 1

--- aula3/arquivos.py
+++ aula3/arquivos.py
@@ -7,11 +7,16 @@ def ex_01():
     ip_validos=[]
     for ip in ips:
         ip_linha = ip.split()
-        ip_campo = ip_linha.split('.')
-
+        for ip_campo in ip_linha:
+            ip_numero = ip_campo.split('.')
+#            ip_numero = [int(i) for i in ip_numero]
+           
+    
 
 
 
 
 
     ips.close
+
+ex_01()
