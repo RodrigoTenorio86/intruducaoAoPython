f34dd450cb0a858d1f80fd90f7b96c03208ab77c
Rodrigo Tenório de Boa Ventura <boaventura19@yahoo.com.br>
2019-12-05 07:44:15 -0800

    Add files via upload
    exercicios de repeticao.

---
+++ repeticao.py
@@ -0,0 +1,73 @@
+def ex_29():
+    valor_valido = True
+    while valor_valido :
+        valor = int(input("Digite uma Nota, entre Zero e Dez "))
+        if(valor >= 0 and valor <= 10):
+            valor_valido = False
+            print("Valor Valido.")
+        else:
+            print("Valor Invalido {}".format(valor))
+
+def ex_39():
+    for i in range(1,11):
+        print("{}".format(i))
+    for i in range(1,11):
+        print("{}".format(i,end=""))
+
+def ex_40():
+    numeros=[]
+    for i in range(0 ,5):
+        numeros.append( float(input("Digite um Numero: ")))
+    print("O maior Numero: {}".format(max(numeros)))
+
+
+def ex_41():
+    numeros = []
+    for i in range(0,5):
+        numeros.append(float(input("Digite numero: ")))
+    print("Soma: {}".format(sum(numeros)))
+    print("Media: {0:.2f}".format(sum(numeros)/len(numeros)))
+
+def ex_42():
+    for i in range(1,51):
+        if(not i%2 == 0):
+            print("Numero Impar {}".format(i))
+
+def ex_44():
+    qtd_turma = int(input("Digite quantidade de Turma: "))
+    qnt_aluno_por_turma = []
+    if(qtd_turma > 0):
+        i = 0
+        while i < qtd_turma:
+            qnt_aluno = int(input("Digite quantidade de alunos nesta Turma: "))
+            if(qnt_aluno <= 40):
+                qnt_aluno_por_turma.append(qnt_aluno)
+                i = i +1
+            else:
+                print("Esta Turma Nao pode ter mais de 40 Alunos.")
+    print(qnt_aluno_por_turma)
+    print("A media de Alunos por Turma e {0:.2f}".format(sum(qnt_aluno_por_turma)/len(qnt_aluno_por_turma)))
+
+
+def ex_45():
+    qtd_cd = int(input("Digite quantidade de CD: "))
+    valor_cada_cd = []
+    if(qtd_cd > 0):
+        for i in range(0 ,qtd_cd):
+            valor_cada_cd.append(float(input("Digite valor do CD: ")))
+    print("Valor Total investido: {0:.2f}".format(sum(valor_cada_cd)))
+    print("Valor Medio: {0:.2f}".format(sum(valor_cada_cd)/len(valor_cada_cd)))
+    
+def ex_46():
+    temperaturas=[]
+    condicao = True
+    while condicao:
+        temperatura = input("Digite valor da Temperatura ou F para fim. ")
+        if(temperatura.upper().strip() != 'F'):
+            temperaturas.append(float(temperatura))
+        else:
+            condicao =False
+    print(temperaturas)
+            
+
+ex_46()
